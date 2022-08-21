3b1c2d461051e739d21ee4af1791644c7904fb48
SceneryMC <1330827323@qq.com>
2022-08-21 21:58:05 +0800

    SceneryMC

---
+++ server/db_operations.h
@@ -0,0 +1,88 @@
+#ifndef H_DB_OPERATIONS
+#define H_DB_OPERATIONS
+
+#include <string>
+#include <mysqlx/xdevapi.h>
+
+constexpr int32_t USER_ID_BEGIN = 10000;
+constexpr int32_t GROUP_ID_BEGIN = 10000;
+typedef int32_t ID;
+
+class LiteChatDatabaseAccess
+{
+public:
+    LiteChatDatabaseAccess(const std::string& url);
+
+    ID userLogin(ID user_id, const std::string& email, const std::string& pwd); 
+    mysqlx::Row getBasicUserDataByID(ID user_id);
+    mysqlx::Row getBasicUserDataByEmail(const std::string& email);
+    mysqlx::Row getBasicGroupData(ID group_id);
+    mysqlx::RowResult searchUser(ID user_id, const std::string& user_name);
+    mysqlx::RowResult searchGroup(ID group_id, const std::string& group_name);
+    mysqlx::RowResult searchUserHistory(ID src_user_id, ID dst_user_id, const std::string& time_begin, const std::string& time_end); 
+    mysqlx::RowResult searchGroupHistory(ID src_user_id, ID dst_group_id, const std::string& time_begin, const std::string& time_end); // 0为群中所有人的发言
+    mysqlx::RowResult searchUnsendMessageFromUser(ID unsend_user_id);
+    mysqlx::RowResult searchUnsendMessageFromGroup(ID unsend_user_id);
+    mysqlx::Row getUserStatus(ID user_id);
+    mysqlx::RowResult getFriendRelation(ID user1_id, ID user2_id);
+    mysqlx::RowResult getFriendRequest(ID user_id);
+    mysqlx::RowResult getGroupMember(ID group_id);
+    mysqlx::RowResult getGroupsOfAUser(ID user_id);
+    
+    ID userRegister(const std::string& user_name, const std::string& pwd, const std::string& email, const std::string& birthday);
+    void addUserHistory(const std::string& send_time, ID src_user_id, ID dst_user_id, const std::string& content);
+    void addGroupHistory(const std::string& send_time, ID src_user_id, ID dst_group_id, const std::string& content);
+    void addUnsendMessageFromUser(const std::string& send_time, ID unsend_user_id, ID src_user_id, const std::string& content);
+    void addUnsendMessageFromGroup(const std::string& send_time, ID unsend_group_id, ID src_user_id, ID dst_group_id, const std::string& content);
+    ID createGroup(const std::string& group_name, ID owner_id, const std::string& group_description);
+    void addUserToGroup(ID group_id, ID user_id);
+    void addGroupToUser(ID user_id, ID group_id);
+    void createFriendRelation(ID user1_id, ID user2_id);
+    void createFriendRequest(ID user_from, ID user_to, const std::string& request_message);
+
+    void updateBasicUserData(ID user_id, const std::string& user_name, const std::string& pwd, const std::string& email, const std::string& birthday, 
+        const std::string& avatar_filename, const std::string& signature);
+    void updateBasicGroupData(ID group_id, const std::string& group_name, ID owner_id, const std::string& group_description);
+    void updateUserStatus(ID user_id, const std::string& datetime);
+
+    void deleteGroup(ID group_id);
+    void removeUserFromGroup(ID group_id, ID user_id);
+    void removeGroupFromUser(ID user_id, ID group_id);
+    void deleteFriendRelation(ID ser1_id, ID user2_id);
+    void deleteFriendRequest(ID user_from, ID user_to);
+
+private:
+    int regsiteredUserCount();
+    bool emailUnique(const std::string& email); 
+    int existedGroupCount();
+    bool groupNameUnique(const std::string& email);
+    mysqlx::RowResult searchUG(mysqlx::TableSelect& table_select, const std::string& type, ID id, const std::string& name);
+    mysqlx::RowResult searchHistory(mysqlx::TableSelect& table_select, const std::string& type,
+        ID src_id, ID dst_id, const std::string& time_begin, const std::string& time_end);
+    mysqlx::RowResult searchUnsendMessage(mysqlx::TableSelect& table_select, ID unsend_user_id);
+    void addHistory(mysqlx::Table& table, const std::string& type, 
+        const std::string& send_time, ID src_id, ID dst_id, const std::string& content);
+
+    mysqlx::Session sess;
+    mysqlx::Schema sch;
+    mysqlx::Table basic_user_data, basic_group_data, user_status, message_to_user, message_to_group, unsend_messgae_from_user, unsend_messgae_from_group,
+        friend_relation, friend_request, group_member, users_group;
+    mysqlx::TableSelect user_login, registered_user_count, existed_group_count,
+        get_basic_user_data, get_basic_group_data,
+        search_user_history, search_group_history, search_unsend_messgae_from_user, search_unsend_messgae_from_group,
+        get_user_status,
+        get_friend_relation, get_friend_request,
+        get_group_member, get_groups_of_a_user;
+
+};
+    // mysqlx::TableInsert user_register,
+    //     add_user_history, add_group_history,
+    //     create_group, add_user_to_group, add_group_to_user,
+    //     create_friend_relation, create_friend_request;
+    // mysqlx::TableUpdate update_basic_user_data, update_basic_group_data,
+    //     update_last_response, update_last_visit_of_user, update_last_visit_of_group;
+    // mysqlx::TableRemove user_delete,
+    //     delete_group, remove_user_from_group, remove_group_from_user,
+    //     delete_friend_relation, delete_friend_request;    
+
+#endif
--- server/sql/command_create_table.sql
+++ server/sql/command_create_table.sql
@@ -7,33 +7,37 @@ user_id INT PRIMARY KEY,
 user_name VARCHAR(32) NOT NULL,
 pwd CHAR(32) NOT NULL,
 email VARCHAR(32) NOT NULL UNIQUE,
-birthday DATE,
-avatar_filename VARCHAR(32),
-signature VARCHAR(128)
+birthday DATE NOT NULL,
+avatar_filename VARCHAR(32) DEFAULT("default.jpg"),
+signature VARCHAR(128) DEFAULT("")
 ); 
+
 CREATE TABLE basic_group_data (
 group_id INT PRIMARY KEY,
 owner_id INT NOT NULL,
 group_name VARCHAR(64) NOT NULL UNIQUE,
-grope_description VARCHAR(256),
+grope_description VARCHAR(256) DEFAULT(""),
 CONSTRAINT OWNER_REGISTERED foreign key (owner_id) references basic_user_data(user_id)
 );
+
 CREATE TABLE user_status (
 id INT PRIMARY KEY,
-last_response DATETIME, -- 储存客户端最后一次回应的时间。当再次上线时，查询send_time > last_response的消息发送给客户端
+is_online BOOL DEFAULT(FALSE),
+handle INT UNIQUE DEFAULT(NULL),-- 储存客户端最后一次回应的时间。当再次上线时，查询send_time > last_response的消息发送给客户端
 -- 其它数据（ip等）？
-CONSTRAINT SRC_USER_REGISTERED foreign key (id) references basic_user_data(user_id)
+CONSTRAINT USER_REGISTERED_1 foreign key (id) references basic_user_data(user_id)
 );
+
 CREATE TABLE message_to_user (
 send_time DATETIME(3), -- 精确到ms
 src_user_id INT,
 dst_user_id INT,
-content VARCHAR(2048) NOT NULL
--- CONSTRAINT IDENTIFIER_MESSAGE_TO_USER PRIMARY KEY(send_time, src_user_id, dst_user_id),
--- CONSTRAINT SRC_USER_REGISTERED_1 foreign key (src_user_id) references basic_user_data(user_id),
--- CONSTRAINT DST_USER_REGISTERED foreign key (dst_user_id) references basic_user_data(user_id)
+content VARCHAR(2048) NOT NULL,
+CONSTRAINT IDENTIFIER_MESSAGE_TO_USER PRIMARY KEY(send_time, src_user_id, dst_user_id),
+CONSTRAINT SRC_USER_REGISTERED_1 foreign key (src_user_id) references basic_user_data(user_id),
+CONSTRAINT DST_USER_REGISTERED_1 foreign key (dst_user_id) references basic_user_data(user_id)
 );
-CREATE INDEX usesrs_pair ON message_to_user(src_user_id ASC, dst_user_id ASC);
+
 CREATE TABLE message_to_group (
 send_time DATETIME(3), -- 精确到ms
 src_user_id INT,
@@ -41,38 +45,63 @@ dst_group_id INT,
 content VARCHAR(2048) NOT NULL,
 CONSTRAINT IDENTIFIER_MESSAGE_TO_USER PRIMARY KEY(send_time, src_user_id, dst_group_id),
 CONSTRAINT SRC_USER_REGISTERED_2 foreign key (src_user_id) references basic_user_data(user_id),
-CONSTRAINT DST_GROUP_CREATED foreign key (dst_group_id) references basic_group_data(group_id)
+CONSTRAINT DST_GROUP_CREATED_2 foreign key (dst_group_id) references basic_group_data(group_id)
 );
-CREATE INDEX usesr_group_pair ON message_to_group(src_user_id ASC, dst_group_id ASC);
+
+CREATE TABLE unsend_message_from_user (
+send_time DATETIME(3), -- 精确到ms
+unsend_user_id INT,
+src_user_id INT,
+content VARCHAR(2048) NOT NULL,
+CONSTRAINT IDENTIFIER_UNSEND_MESSGAE_FROM_USER PRIMARY KEY(send_time, unsend_user_id, src_user_id),
+CONSTRAINT SRC_USER_REGISTERED_3 foreign key (src_user_id) references basic_user_data(user_id),
+CONSTRAINT UNSEND_USER_REGISTERED_3 foreign key (unsend_user_id) references basic_user_data(user_id)
+);
+CREATE INDEX unsend_users_pair ON unsend_message_from_user(unsend_user_id ASC, send_time ASC);
+
+CREATE TABLE unsend_message_from_group (
+send_time DATETIME(3), -- 精确到ms
+unsend_user_id INT,
+src_user_id INT,
+dst_group_id INT,
+content VARCHAR(2048) NOT NULL,
+CONSTRAINT IDENTIFIER_UNSEND_MESSGAE_FROM_GROUP PRIMARY KEY(send_time, unsend_user_id, src_user_id, dst_group_id),
+CONSTRAINT UNSEND_USER_REGISTERED_4 foreign key (unsend_user_id) references basic_user_data(user_id),
+CONSTRAINT SRC_USER_REGISTERED_4 foreign key (src_user_id) references basic_user_data(user_id),
+CONSTRAINT DST_USER_REGISTERED_4 foreign key (dst_group_id) references basic_group_data(group_id)
+);
+CREATE INDEX unsend_user_group_pair ON unsend_message_from_group(unsend_user_id ASC, send_time ASC);
+
 CREATE TABLE friend_ralation ( -- 为加快查找速度，可以同时插入两对：(1,2)(2,1)
 user1_id INT, 
 user2_id INT,
-last_visit DATETIME(3) DEFAULT(NULL), 
 -- 其它属性？（插入两对后，属性只需要描述一个方向；）
 CONSTRAINT IDENTIFIER_FRIEND_RELATION PRIMARY KEY(user1_id, user2_id),
 CONSTRAINT USER1_REGISTERED foreign key (user1_id) references basic_user_data(user_id),
 CONSTRAINT USER2_REGISTERED foreign key (user2_id) references basic_user_data(user_id)
 );
+
 CREATE TABLE friend_request ( -- 处理后删除，未处理则考虑一直发送？
 user_from INT,
 user_to INT,
 request_message VARCHAR(128),
 CONSTRAINT IDENTIFIER_FRIEND_REQUEST PRIMARY KEY(user_from, user_to)
 );
+
 CREATE TABLE group_member (
 group_id INT,
 member_id INT,
 -- 其它属性？
 CONSTRAINT IDENTIFIER_GROUP_MEMBER PRIMARY KEY(group_id, member_id),
-CONSTRAINT GROUP_CREATED_1 foreign key (group_id) references basic_group_data(group_id),
+CONSTRAINT GROUP_CREATED_5 foreign key (group_id) references basic_group_data(group_id),
 CONSTRAINT MEMBER_REGISTERED foreign key (member_id) references basic_user_data(user_id)
 );
+
 CREATE TABLE users_group ( -- 为加快查找速度，维护两个表
 user_id INT,
 group_id INT,
-last_visit DATETIME(3), 
 -- 其它属性？
 CONSTRAINT IDENTIFIER_GROUP_MEMBER PRIMARY KEY(user_id, group_id),
-CONSTRAINT GROUP_CREATED_2 foreign key (group_id) references basic_group_data(group_id),
-CONSTRAINT USER_REGISTERED foreign key (user_id) references basic_user_data(user_id)
+CONSTRAINT GROUP_CREATED_6 foreign key (group_id) references basic_group_data(group_id),
+CONSTRAINT USER_REGISTERED_2 foreign key (user_id) references basic_user_data(user_id)
 );
