ebd2aac7fc8f96e19a306e63d3177029722babfe
SceneryMC <1330827323@qq.com>
2022-08-23 15:22:23 +0800

    中间结果

--- server/linux_client_test/main.cpp
+++ server/linux_client_test/main.cpp
@@ -7,7 +7,7 @@
 #include <sys/socket.h>
 #include "json.hpp"
 #include <unistd.h>
-#include <cstdlib> 
+#include <cstdlib>
 
 using json = nlohmann::json;
 
@@ -16,90 +16,67 @@ using json = nlohmann::json;
 int main(){
     int sock0 = socket(AF_INET, SOCK_STREAM, 0);
     int sock1 = socket(AF_INET, SOCK_STREAM, 0);
-  
+
     struct sockaddr_in serv_addr;
-    memset(&serv_addr, 0, sizeof(serv_addr));  
-    serv_addr.sin_family = AF_INET;  
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);  
-    serv_addr.sin_port = htons(2333); 
-    connect(sock0, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-    connect(sock1, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(2333);
+    connect(sock0, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    connect(sock1, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
 
     json message, data;
     std::string final_message;
 
-    message["type"] = 1001;
-    data["username"] = "dqwdqd";
-    data["pwd"] = "00000000000000000000000000000000";
-    data["email"] = "ne#1231.com";
-    data["birthday"] = "2001-08-02";
-    message["data"] = data;
-    final_message = to_string(message);
+    final_message = R"()";
     send(sock0, final_message.c_str(), final_message.size(), 0);
     usleep(100000);
 
-    message["type"] = 1000;
-    data["email"] = "ne#1231.com";
-    data["pwd"] = "00000000000000000000000000000000";
-    message["data"] = data;
-    final_message = to_string(message);
+    final_message = R"({
+    "type":1001,
+    "data":{
+        "username":"dqwdqd",
+        "pwd":"00000000000000000000000000000000",
+        "email":"ne#1231.com",
+        "birthday":"2001-08-02"
+    }
+})";
+    // register 10000
     send(sock0, final_message.c_str(), final_message.size(), 0);
     usleep(100000);
 
-    message["type"] = 1001;
-    data["username"] = "abab";
-    data["pwd"] = "00000000000000000000000000000000";
-    data["email"] = "2333.com";
-    data["birthday"] = "2001-08-02";
-    message["data"] = data;
-    final_message = to_string(message);
+    final_message = R"({
+    "type":1000,
+    "data":{
+        "pwd":"00000000000000000000000000000000",
+        "email":"ne#1231.com"
+    }
+})";
+    // login 10000
+    send(sock0, final_message.c_str(), final_message.size(), 0);
+    usleep(100000);
+
+    final_message = R"({
+    "type":1001,
+    "data":{
+        "username":"abab",
+        "pwd":"00000000000000000000000000000000",
+        "email":"2333.com",
+        "birthday":"2000-08-02"
+    }
+})";
+    // register 10001
     send(sock1, final_message.c_str(), final_message.size(), 0);
     usleep(100000);
 
-    message["type"] = 1000;
-    data["user_id"] = 10001;
-    data["pwd"] = "00000000000000000000000000000000";
-    message["data"] = data;
-    final_message = to_string(message);
-    send(sock1, final_message.c_str(), final_message.size(), 0);
-    usleep(100000);
-    
     final_message = R"({
-    "type":1008,
+    "type":1000,
     "data":{
-        "keyword":"10001"
-    }
-})";
-    send(sock0, final_message.c_str(), final_message.size(), 0);
-    usleep(100000);
-
-    final_message = R"({
-    "type":1009,
-    "data":{
-        "user_id":10000,
-        "to_id":10001
-    }
-})";
-    send(sock0, final_message.c_str(), final_message.size(), 0);
-    usleep(100000);
-
-    final_message = R"({
-    "type":1010,
-    "data":{
+        "pwd":"00000000000000000000000000000000",
         "user_id":10001
     }
 })";
-    send(sock1, final_message.c_str(), final_message.size(), 0);
-    usleep(100000);
-
-    final_message = R"({
-    "type":1011,
-    "data":{
-        "from_id":10000,
-        "user_id":10001,
-        "accept":false
-    }
-})";
+    // login 10001
     send(sock1, final_message.c_str(), final_message.size(), 0);
     usleep(100000);
 
@@ -110,37 +87,7 @@ int main(){
         "to_id":10001
     }
 })";
-    send(sock0, final_message.c_str(), final_message.size(), 0);
-    usleep(100000);
-    
-    final_message = R"({
-    "type":1011,
-    "data":{
-        "from_id":10000,
-        "user_id":10001,
-        "accept":true
-    }
-})";
-    send(sock1, final_message.c_str(), final_message.size(), 0);
-    usleep(100000);
-
-    final_message = R"({
-    "type":1012,
-    "data":{
-        "user_id":10001,
-        "friend_id":10000
-    }
-})";
-    send(sock1, final_message.c_str(), final_message.size(), 0);
-    usleep(100000);
-
-    final_message = R"({
-    "type":1009,
-    "data":{
-        "user_id":10000,
-        "to_id":10001
-    }
-})";
+    // 10000 add 10001
     send(sock0, final_message.c_str(), final_message.size(), 0);
     usleep(100000);
 
@@ -152,7 +99,18 @@ int main(){
         "accept":true
     }
 })";
+    // 10001 accept 10000
     send(sock1, final_message.c_str(), final_message.size(), 0);
+    usleep(100000);
+
+    final_message = R"({
+    "type":1004,
+    "data":{
+        "user_id":10000
+    }
+})";
+    // get 10000's all friends
+    send(sock0, final_message.c_str(), final_message.size(), 0);
     usleep(100000);
 
     final_message = R"({
@@ -160,31 +118,98 @@ int main(){
     "data":{
         "user_id":10000,
         "to_id":10001,
-        "time":"2022-01-01",
+        "time":"2022-01-01 01:31:00",
         "content":"hello"
     }
 })";
+    // 10000 send private message to 10001
     send(sock0, final_message.c_str(), final_message.size(), 0);
     usleep(100000);
 
     final_message = R"({
-    "type":1004,
+    "type":1002,
     "data":{
+        "user_id":10000,
+        "to_id":10001,
+        "time":"2022-01-01 01:32:00",
+        "content":"I know you're online"
+    }
+})";
+    // 10000 send private message to 10001
+    send(sock0, final_message.c_str(), final_message.size(), 0);
+    usleep(100000);
+
+    final_message = R"({
+    "type":1013,
+    "data":{
+        "user_id":10001
+})";
+    send(sock1, final_message.c_str(), final_message.size(), 0);
+    usleep(100000);
+    // 10001 logout
+
+    final_message = R"({
+    "type":1002,
+    "data":{
+        "user_id":10000,
+        "to_id":10001,
+        "time":"2022-01-01 01:33:00",
+        "content":"hey, quickly reply"
+    }
+})";
+    // 10000 send private message to 10001
+    send(sock0, final_message.c_str(), final_message.size(), 0);
+    usleep(100000);
+
+    final_message = R"({
+    "type":1002,
+    "data":{
+        "user_id":10000,
+        "to_id":10001,
+        "time":"2022-01-01 01:34:00",
+        "content":"are you kidding me?"
+    }
+})";
+    // 10000 send private message to 10001
+    send(sock0, final_message.c_str(), final_message.size(), 0);
+    usleep(100000);
+
+    final_message = R"({
+    "type":1000,
+    "data":{
+        "pwd":"00000000000000000000000000000000",
         "user_id":10001
     }
 })";
+    // login 10001
     send(sock1, final_message.c_str(), final_message.size(), 0);
+    usleep(100000);
 
+    final_message = R"({
+    "type":1006,
+    "data":{
+        "user_id":10001,
+        "from_id":10000,
+        "begin_time":"2022-01-01 01:30:00",
+        "end_time":"2022-01-01 02:34:00"
+    }
+})";
+    // 10001 get history message from 10000
+    send(sock1, final_message.c_str(), final_message.size(), 0);
+    usleep(100000);
 
-//     final_message = R"({
-//     "type":1006,
-//     "data":{
-//         ""
-//     }
-// })";
-//     send(sock1, final_message.c_str(), final_message.size(), 0);
-//     usleep(100000);
-
+    final_message = R"({
+    "type":1006,
+    "data":{
+        "user_id":10001,
+        "from_id":10000,
+        "begin_time":"2022-01-01 01:32:00",
+        "end_time":"2022-01-01 02:34:00"
+    }
+})";
+    // 10001 get history message from 10000
+    send(sock1, final_message.c_str(), final_message.size(), 0);
+    usleep(100000);
 
     getchar();
 
--- server/sql/test_1.sql
+++ server/sql/test_1.sql
@@ -1,18 +1,10 @@
--- DELETE FROM message_to_user;
--- DELETE FROM unsend_message_from_user;
--- DELETE FROM unsend_message_from_group;
--- DELETE FROM friend_relation;
--- DELETE FROM friend_request;
--- DELETE FROM group_member;
--- DELETE FROM group_request;
--- DELETE FROM basic_group_data;
--- DELETE FROM user_status;
--- DELETE FROM basic_user_data;
-
-select * FROM message_to_user; 
-select * from user_status;
-select * from basic_group_data;
-select * from friend_relation;
-select * from friend_request;
-select * from group_request;
-select * from unsend_message_from_user;
+DELETE FROM message_to_user;
+DELETE FROM unsend_message_from_user;
+DELETE FROM unsend_message_from_group;
+DELETE FROM friend_relation;
+DELETE FROM friend_request;
+DELETE FROM group_member;
+DELETE FROM group_request;
+DELETE FROM basic_group_data;
+DELETE FROM user_status;
+DELETE FROM basic_user_data;
